ef310d38cd078cef96b0c1c0c1dcf5f764507a4e
igorrc <igor.ramos.cruzado.w@gmail.com>
2025-12-07 11:52:53 -0500

    feat : create global error

---
+++ src/main/java/com/gniuscode/sie/management/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.gniuscode.sie.management.common.exception;
+
+import com.gniuscode.sie.management.common.dto.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Map<String, String>>> handleValidationExceptions(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+           String fieldName = ((FieldError)error).getField();
+           String errorMessage = error.getDefaultMessage();
+           errors.put(fieldName,errorMessage);
+        });
+        return new ResponseEntity<>(
+                ApiResponse.error("Validation field",errors),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse<String>> handleResourceNotFoundException(ResourceNotFoundException ex){
+        return new ResponseEntity<>(
+                ApiResponse.error(ex.getMessage(), null),
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<String>> handleGlobalException(Exception ex){
+        return new ResponseEntity<>(
+                ApiResponse.error("Internal Server Error: " + ex.getMessage(), null),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
