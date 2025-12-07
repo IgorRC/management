474c67f7b5bccee66c9d75be059cd2673ae5af16
igorrc <igor.ramos.cruzado.w@gmail.com>
2025-12-07 11:33:17 -0500

    feat : add dependency in project for common and security

---
+++ src/main/java/com/gniuscode/sie/management/common/dto/ApiResponse.java
@@ -0,0 +1,30 @@
+package com.gniuscode.sie.management.common.dto;
+
+import java.time.LocalDateTime;
+
+
+public class ApiResponse <T>{
+
+    private boolean success;
+    private String message;
+    private T data;
+    private LocalDateTime timestamp;
+
+    public ApiResponse(String message, T data) {
+        this.success = true;
+        this.message = message;
+        this.data = data;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public ApiResponse(String message){
+        this.success = true;
+        this.message = message;
+        this.data = null;
+        this.timestamp = LocalDateTime.now();
+    }
+
+//    public static <T> ApiResponse<T> error(String message, T data){
+//        return new ApiResponse<>(false,message,data,LocalDateTime.now());
+//    }
+}
