5da37aab223f10ac63421e517f1a6d3ba234779c
igorrc <igor.ramos.cruzado.w@gmail.com>
2025-12-08 00:09:06 -0500

    feat: libs  permission

--- src/main/java/com/gniuscode/sie/management/security/entity/Permission.java
+++ src/main/java/com/gniuscode/sie/management/security/entity/Permission.java
@@ -1,17 +1,21 @@
 package com.gniuscode.sie.management.security.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
+@Entity
+@Table(name = "permissions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Permission {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
     private String name;
-
 }
