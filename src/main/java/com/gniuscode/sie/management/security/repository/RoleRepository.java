171ea3b5591f641a91939562e6a06f4fe1fbe671
igorrc <igor.ramos.cruzado.w@gmail.com>
2025-12-11 01:41:43 -0500

    feat : create data layer for role

---
+++ src/main/java/com/gniuscode/sie/management/security/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.gniuscode.sie.management.security.repository;
+
+import com.gniuscode.sie.management.security.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
