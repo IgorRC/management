09aef97cfb1d49905cd82576192934727655bf25
igorrc <igor.ramos.cruzado.w@gmail.com>
2025-12-11 01:49:48 -0500

    feat : create date register

---
+++ src/main/java/com/gniuscode/sie/management/security/dto/RegisterRequest.java
@@ -0,0 +1,25 @@
+package com.gniuscode.sie.management.security.dto;
+
+import com.gniuscode.sie.management.security.entity.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import java.util.Set;
+
+@Data
+public class RegisterRequest {
+    @NotNull(message = "Username is required")
+    @Size(min = 3, max = 20,message = "Username must be between 3 and 20 characters")
+    private String username;
+
+    @NotNull(message = "Password id required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String email;
+
+    @NotNull(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String password;
+
+    private Set<Role> roles;
+}
