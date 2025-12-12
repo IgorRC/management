4c0da5ce2538f19379eb6c700c56bb570bcbd25e
igorrc <igor.ramos.cruzado.w@gmail.com>
2025-12-12 00:36:06 -0500

    feat : creator token service

---
+++ src/main/java/com/gniuscode/sie/management/security/service/JwtService.java
@@ -0,0 +1,81 @@
+package com.gniuscode.sie.management.security.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.expiration-time}")
+    private long jwtExpiration;
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return buildToken(extraClaims, userDetails, jwtExpiration);
+    }
+
+    private String buildToken(Map<String, Object> extraClaims, UserDetails userDetails, long jwtExpiration) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSingInkey(), SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return exctractClaim(token, Claims::getExpiration);
+    }
+
+    public String extractUsername(String token) {
+        return exctractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T exctractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extracAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extracAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSingInkey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSingInkey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
+// dormiste falta agregar mas funconaes de la seguridad
