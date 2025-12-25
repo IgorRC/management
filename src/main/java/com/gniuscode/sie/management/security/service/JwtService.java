2a245e286b99f0206dcc1529d09c0d3aa8796124
igorrc <igor.ramos.cruzado.w@gmail.com>
2025-12-25 03:19:42 -0500

    fix : change algorithm standar

--- src/main/java/com/gniuscode/sie/management/security/service/JwtService.java
+++ src/main/java/com/gniuscode/sie/management/security/service/JwtService.java
@@ -38,7 +38,7 @@ public class JwtService {
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
-                .signWith(getSingInkey(), SignatureAlgorithm.ES256)
+                .signWith(getSingInkey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
