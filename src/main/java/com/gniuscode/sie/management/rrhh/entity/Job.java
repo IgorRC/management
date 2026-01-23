1e0ee4e0ed35b0c9bb28d2da690b57950867fdd3
igorrc <igor.ramos.cruzado.w@gmail.com>
2026-01-23 09:17:48 -0500

    refactor : add notacion Job

--- src/main/java/com/gniuscode/sie/management/rrhh/entity/Job.java
+++ src/main/java/com/gniuscode/sie/management/rrhh/entity/Job.java
@@ -1,10 +1,15 @@
 package com.gniuscode.sie.management.rrhh.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "jobs", schema = "rrhh")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Job {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
